092500ca1aae82b6255260c7d2db1281da4ba113
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-16 00:20:16 +0100

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
-  basePath: '/details',
-  // output: "export",  // <=== enables static exports
-  reactStrictMode: false,
-  images: {
-    unoptimized: true,
-    domains: ['media.graphassets.com', 'lh3.googleusercontent.com']
+  reactStrictMode:false,
+  images:{
+      unoptimized:true,
+      domains:['media.graphassets.com','lh3.googleusercontent.com']
   }
 };
 
