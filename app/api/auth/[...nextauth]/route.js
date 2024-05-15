23ad758c1c23a7ddd1779392b702c00d4d4674c5
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-16 00:26:14 +0100

    auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -22,6 +22,7 @@ export const authOptions = {
             }
         },
     }],
+    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
         async jwt({token, account, profile}) {
             if (account) {
