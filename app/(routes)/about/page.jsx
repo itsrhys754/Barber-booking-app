fe69c975ab5b10c4234eb06406701950ae5c4166
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-16 17:37:06 +0100

    about

--- app/(routes)/about/page.jsx
+++ app/(routes)/about/page.jsx
@@ -35,34 +35,9 @@ function MyBooking() {
 
     return (
         <div className='my-10 mx-5 md:mx-36'>
-           <h2 className='font-bold text-[20px] my-2'>My Bookings</h2>
-            <Tabs defaultValue="booked" className="w-full">
-                <TabsList className="w-full justify-start">
-                    <TabsTrigger value="booked">Booked</TabsTrigger>
-                    <TabsTrigger value="completed">Completed</TabsTrigger>
-                    <TabsTrigger value="completed">Cancelled</TabsTrigger>
-
-                </TabsList>
-                <TabsContent value="booked">
-                    <BookingHistoryList 
-                    bookingHistory={filterData('booked')}
-                    type='booked'
-                    />
-                </TabsContent>
-                <TabsContent value="completed">
-                <BookingHistoryList 
-                bookingHistory={filterData('completed')}
-                type='completed'/>
-                    
-                </TabsContent>
-                
-                <TabsContent value="cancelled">
-                <BookingHistoryList 
-                bookingHistory={filterData('cancelled')}
-                type='cancelled'/>
-                    
-                </TabsContent>
-            </Tabs>
+           <h2 className='font-bold text-[20px] my-2'>About us</h2>
+           <p>Coming soon!</p>
+           
 
         </div>
     )
