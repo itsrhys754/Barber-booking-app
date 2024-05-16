22b36875a1f85a29071c66598be02fa442e6837d
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-17 00:05:36 +0100

    navbar first change

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,82 +1,65 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { signIn, signOut, useSession } from 'next-auth/react'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import Link from 'next/link'
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link'; // Import Link component
+
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const {data}=useSession();
-
-  useEffect(()=>{
-    console.log(data);
-  },[data])
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <div className='p-5 shadow-sm flex  justify-between
-    '>
-        <div className='flex items-center gap-8 '>
-        <Link href="/">
-            <Image src='/logo.svg' alt='logo'
-            width={180} height={100} />
-            </Link>
-            <div className='md:flex items-center
-            gap-6 hidden
-            '>
-                <Link href={'/'} className='hover:scale-105 hover:text-primary
-                cursor-pointer'>Home</Link>
-                <Link href={'/services'} className='hover:scale-105 hover:text-primary
-                cursor-pointer'>Services</Link>
-                <Link href={'/about'} className='hover:scale-105 hover:text-primary
-                cursor-pointer'>About us</Link>
-                <Link href={'https://blackwoodbarber.setmore.com/beta'} className='hover:scale-105 hover:text-primary
-                cursor-pointer'>Book</Link>
-
-
-
-            </div>
-           
-        </div>
-        {/* <div>
-          {data?.user?
-          
-          <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-  <Image src={data?.user?.image}
-          alt='user'
-          width={40}
-          height={40}
-          className='rounded-full'
-          />
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>
-     <Link href={'/mybooking'}>My Booking</Link> 
-      </DropdownMenuItem>
-    <DropdownMenuItem onClick={()=>signOut()}>Logout</DropdownMenuItem> */}
-   
-  {/* </DropdownMenuContent>
-</DropdownMenu>
-
-          :  
-
-          <Button onClick={()=>signIn('descope')}>Login / Sign Up</Button>
-
-        }
-            </div> */}
+    <div className='p-5 shadow-sm flex justify-between'>
+      <div className='flex items-center gap-8'>
+        {/* Your logo and navigation links */}
+      </div>
+      <div className='md:hidden'>
+        <button onClick={toggleMenu}>
+          {/* Icon for menu */}
+        </button>
+        {isMenuOpen && (
+          <div className='absolute top-16 right-5 bg-white p-4 shadow-md'>
+            <ul>
+              <li>
+                <Link href='/'>Home</Link>
+              </li>
+              <li>
+                <Link href='/services'>Services</Link>
+              </li>
+              <li>
+                <Link href='/about'>About us</Link>
+              </li>
+              <li>
+                <Link href='https://blackwoodbarber.setmore.com/beta'>Book</Link>
+              </li>
+              {/* Check session and show appropriate items */}
+            </ul>
+          </div>
+        )}
+      </div>
+      {/* Render menu for larger screens */}
+      <div className='hidden md:block'>
+        <ul className='flex gap-6'>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/services'>Services</Link>
+          </li>
+          <li>
+            <Link href='/about'>About us</Link>
+          </li>
+          <li>
+            <Link href='https://blackwoodbarber.setmore.com/beta'>Book</Link>
+          </li>
+          {/* Check session and show appropriate items */}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
