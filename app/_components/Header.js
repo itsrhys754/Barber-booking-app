b4359d4fefd1138e46e607a758e667f7a4c6ca41
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-17 00:26:27 +0100

    final navbar changes

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -53,44 +53,44 @@ function Header() {
             Book
           </Link>
         </div>
-        <div className="md:hidden ml-auto">
-          <button
-            id="menu-toggle"
-            className="text-gray-700 hover:text-primary focus:outline-none"
-            onClick={toggleMenu}
-          >
-            {showMenu ? (
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            ) : (
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            )}
-          </button>
-        </div>
+      </div>
+      <div className="md:hidden">
+        <button
+          id="menu-toggle"
+          className="text-gray-700 hover:text-primary focus:outline-none"
+          onClick={toggleMenu}
+        >
+          {showMenu ? (
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              className="h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          )}
+        </button>
       </div>
       <div
         className={`md:hidden ${
