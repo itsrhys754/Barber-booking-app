19379fc274f8127e35150401e1dd9157fdc038ef
Rhys Gregory <rhysgregory90@gmail.com>
2024-05-17 00:12:28 +0100

    toggle button for mobile menu

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,63 +1,111 @@
 "use client";
-
-import { useState } from 'react';
-import Link from 'next/link'; // Import Link component
-
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data } = useSession();
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setShowMenu(!showMenu);
   };
 
   return (
-    <div className='p-5 shadow-sm flex justify-between'>
-      <div className='flex items-center gap-8'>
-        {/* Your logo and navigation links */}
+    <div className="p-5 shadow-sm flex justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={180} height={100} />
+        </Link>
+        <div
+          className={`mobile-menu ${
+            showMenu ? "flex" : "hidden"
+          } md:flex flex-col md:flex-row items-center gap-6`}
+        >
+          <Link
+            href={"/"}
+            className="hover:scale-105 hover:text-primary cursor-pointer"
+          >
+            Home
+          </Link>
+          <Link
+            href={"/services"}
+            className="hover:scale-105 hover:text-primary cursor-pointer"
+          >
+            Services
+          </Link>
+          <Link
+            href={"/about"}
+            className="hover:scale-105 hover:text-primary cursor-pointer"
+          >
+            About us
+          </Link>
+          <Link
+            href={"https://blackwoodbarber.setmore.com/beta"}
+            className="hover:scale-105 hover:text-primary cursor-pointer"
+          >
+            Book
+          </Link>
+        </div>
+        <div className="md:hidden flex items-center">
+          <button
+            id="menu-toggle"
+            className="text-gray-700 hover:text-gray-900 focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <svg
+              className="h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
-      <div className='md:hidden'>
-        <button onClick={toggleMenu}>
-          {/* Icon for menu */}
-        </button>
-        {isMenuOpen && (
-          <div className='absolute top-16 right-5 bg-white p-4 shadow-md'>
-            <ul>
-              <li>
-                <Link href='/'>Home</Link>
-              </li>
-              <li>
-                <Link href='/services'>Services</Link>
-              </li>
-              <li>
-                <Link href='/about'>About us</Link>
-              </li>
-              <li>
-                <Link href='https://blackwoodbarber.setmore.com/beta'>Book</Link>
-              </li>
-              {/* Check session and show appropriate items */}
-            </ul>
-          </div>
+      {/* <div>
+        {data?.user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Image
+                src={data?.user?.image}
+                alt="user"
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href={"/mybooking"}>My Booking</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => signOut()}>
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button onClick={() => signIn("descope")}>Login / Sign Up</Button>
         )}
-      </div>
-      {/* Render menu for larger screens */}
-      <div className='hidden md:block'>
-        <ul className='flex gap-6'>
-          <li>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <Link href='/services'>Services</Link>
-          </li>
-          <li>
-            <Link href='/about'>About us</Link>
-          </li>
-          <li>
-            <Link href='https://blackwoodbarber.setmore.com/beta'>Book</Link>
-          </li>
-          {/* Check session and show appropriate items */}
-        </ul>
-      </div>
+      </div> */}
     </div>
   );
 }
